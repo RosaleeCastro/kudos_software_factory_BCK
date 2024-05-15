4fa2f0c1d071cc60ed386accd7e4b850ef37771b
RosaleeCastro <rosalee.castrot@gmail.com>
2024-05-15 17:53:12 -0500

    Add initial routers

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
 import express from "express"
+import authRouter from "./routes/auth-router";
 const morgan = require('morgan')
 
 const app = express();
 const port = 3000;
 
+// Middleware
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    console.log(`${req.url} esta es la ruta solicitada`)
-    res.send('Hello World!')
-})
+
+//Routers
+app.use(authRouter)
 
 app.listen (port, () => {
    console.log(`Listening on port ${port}`) 
 })
+
+
+// Aqui tenemos:
+// Creación del servidor puerto 3000
+// Usamos el middlewear de json y el middlewear de morgan ....+
+// Definimos las rutas de la Api
+// finalmente escuchar el puerto y la ruta solicitada
---
+++ src/routes/auth-router.ts
@@ -0,0 +1,21 @@
+import express from "express";
+
+
+
+const authRouter= express.Router();
+
+authRouter.post("/login", (req, res) => {})
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default authRouter;
