14e0cde2c381c431b140070063bc61d5a55161c7
RosaleeCastro <rosalee.castrot@gmail.com>
2024-05-15 10:59:38 -0500

    Add. middlewears

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import express from "express"
+const morgan = require('morgan')
 
 const app = express();
 const port = 3000;
 
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    console.log(`${req.url} esta es la ruta solicitada`)
+    res.send('Hello World!')
+})
 
 app.listen (port, () => {
    console.log(`Listening on port ${port}`) 
