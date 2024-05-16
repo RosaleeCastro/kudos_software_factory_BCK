499220472a7963ab3508bbc262a728c1b30cef49
RosaleeCastro <rosalee.castrot@gmail.com>
2024-05-15 20:18:21 -0500

    Add table users from migrations

---
+++ src/db/migrations/2024.05.15T20.08.20.create-users.ts
@@ -0,0 +1,17 @@
+import { Migration } from "../scripts/dbMigrate";
+
+
+
+export const up: Migration = async (params) => {
+    return params.context.query(`CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL CHECK (email ~* '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'),
+        age INTEGER CHECK (age > 0),
+        role VARCHAR(10) DEFAULT 'user' CHECK (role IN ('user', 'admin'))
+    )`);
+};
+
+export const down: Migration = async (params) => {
+    return params.context.query(`DROP TABLE users`);
+}
